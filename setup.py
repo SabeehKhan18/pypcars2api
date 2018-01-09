0612c7907701524e78d090d9b481ee07ee057acb
marcelomanzo <marcelomanzo@users.noreply.github.com>
2018-01-09 10:30:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='carseour',
+setup(name='pypcars2api',
       version='0.1',
       description='Project Cars 2 API implementation for Python 3',
       author='Marcelo Manzo',
       author_email='marcelomanzo@gmail.com',
-      url='https://github.com/marcelomanzo/carseour/',
+      url='https://github.com/marcelomanzo/pypcars2api/',
       keywords='games racing projectcars',
       packages=find_packages(),      
      )
