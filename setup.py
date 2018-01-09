3be6baddc95dbba0f527abd5061bcd7f52cd2261
marcelomanzo <marcelomanzo@users.noreply.github.com>
2018-01-09 10:22:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup, find_packages
 
 setup(name='carseour',
-      version='0.2',
-      description='Project Cars API implementation for Python 3',
-      author='Mats Lindh',
-      author_email='mats@lindh.no',
-      url='https://github.com/matslindh/carseour/',
+      version='0.1',
+      description='Project Cars 2 API implementation for Python 3',
+      author='Marcelo Manzo',
+      author_email='marcelomanzo@gmail.com',
+      url='https://github.com/marcelomanzo/carseour/',
       keywords='games racing projectcars',
       packages=find_packages(),      
      )
